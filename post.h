f84fd440d1645168b40246fc9d49aaec0c1071d4
tshech <tomshech8@gmail.com>
2020-08-31 17:44:31 +0300

    final commit

--- post.h
+++ post.h
@@ -9,9 +9,14 @@
 
 typedef struct post *Post;
 
+//gets key, number of comments and set of posts
+//insert the post to a set of all posts
+//returns the post
 Post create_post(int key, int comments, struct Set* posts);
 
+//delete a post
 void delete_post(Post post);
 
+//gets a set of posts and return the most commented post
 int get_best_post(struct Set* posts);
 #endif //INC_236346_POST_H
